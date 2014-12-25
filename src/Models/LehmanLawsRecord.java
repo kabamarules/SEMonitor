66b4ea4334113b5178b9a7200895a4a574037086
Stathis Fotiadis <sfotiadis@gmail.com>
2014-12-25 17:37:55 +0100

    Fix white spaces

--- src/Models/LehmanLawsRecord.java
+++ src/Models/LehmanLawsRecord.java
@@ -6,35 +6,39 @@ public class LehmanLawsRecord {
 	 * Holds the validity of the law
 	 */
 	private boolean validity;
+	
 	/**
 	 * Holds the comment for the law
 	 */
 	private String comment;
+	
 	/**
 	 * Validity getter
 	 */
 	public boolean getValidity() {
 	 	 return validity; 
 	}
+	
 	/**
 	 * Validity setter
 	 */
 	public void setValidity(boolean validity) { 
 		 this.validity = validity; 
 	}
+	
 	/**
 	 * Comment getter
 	 */
 	public String getComment() {
 	 	 return comment; 
 	}
+	
 	/**
 	 * Comment setter
 	 */
 	public void setComment(String comment) { 
 		 this.comment = comment; 
 	}
-
 
 	/**
 	 * Constructor
