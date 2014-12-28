fd4dfd573fab06f7718c708111a469eaa8b4f6e1
Stathis Fotiadis <sfotiadis@gmail.com>
2014-12-28 14:33:21 +0100

    Removing unused methods in View

--- src/main/Views/View.java
+++ src/main/Views/View.java
@@ -9,24 +9,6 @@ public abstract class View extends JPanel {
 
 	protected Presenter callbackPresenter;
 
-	/**
-	 * Getter of callbackPresenter
-	 */
-	public Presenter getCallbackPresenter() {
-	 	 return callbackPresenter; 
-	}
-
-	/**
-	 * Setter of callbackPresenter
-	 */
-	public void setCallbackPresenter(Presenter callbackPresenter) { 
-		 this.callbackPresenter = callbackPresenter; 
-	}
-
-	/**
-	 * 
-	 * @param callbackPresenter 
-	 */
 	public View(Presenter callbackPresenter) { 
 		 this.callbackPresenter = callbackPresenter; 
 	 }
