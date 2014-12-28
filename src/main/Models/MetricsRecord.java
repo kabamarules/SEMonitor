a593e2e5d83918855fd48e15c576f309743cf335
Stathis Fotiadis <sfotiadis@gmail.com>
2014-12-28 12:25:16 +0100

    Rearrangin and removing unused setter of MetricsRecord

--- src/main/Models/MetricsRecord.java
+++ src/main/Models/MetricsRecord.java
@@ -8,50 +8,32 @@ public class MetricsRecord {
 	 * A data structure that holds the changes of the release
 	 */
 	private ChangeRecord changeRecord;
-	/**
-	 * The previous total of the changes
-	 */
+
 	private int previousTotal;
-	/**
-	 * The previous release date
-	 */
 	private Date previousDate;
 	
 	/**
-	 * Getter of changeRecord
+	 * Constructor
+	 * @param cr ChangeRecord
+	 * @param prFun Previous total of changes
+	 * @param prDate Previous date
 	 */
+	public MetricsRecord(ChangeRecord cr, int prTot, Date prDate) { 
+		this.changeRecord = cr;
+		this.previousTotal = prTot;
+		this.previousDate = prDate;
+	 }
+	
 	public ChangeRecord getChangeRecord() {
 	 	 return changeRecord; 
 	}
-	/**
-	 * Setter of changeRecord
-	 */
-	public void setChangeRecord(ChangeRecord changeRecord) { 
-		 this.changeRecord = changeRecord; 
-	}
-	/**
-	 * Getter of previousTotal
-	 */
+
 	public int getPreviousTotal() {
 	 	 return previousTotal; 
 	}
-	/**
-	 * Setter of previousTotal
-	 */
-	public void setPreviousTotal(int previousTotal) { 
-		 this.previousTotal = previousTotal; 
-	}
-	/**
-	 * Getter of previousDate
-	 */
+
 	public Date getPreviousDate() {
 	 	 return previousDate; 
-	}
-	/**
-	 * Setter of previousDate
-	 */
-	public void setPreviousDate(Date previousDate) { 
-		 this.previousDate = previousDate; 
 	}
 
 	/**
@@ -100,16 +82,4 @@ public class MetricsRecord {
 		return "Total/Growth/Compl/TaskRate: " + getTotalNumber() + "," + getGrowthRate() + "," + getComplexity() +
 				"," + getTaskRate();
 	}
-
-	/**
-	 * Constructor
-	 * @param cr ChangeRecord
-	 * @param prFun Previous total of changes
-	 * @param prDate Previous date
-	 */
-	public MetricsRecord(ChangeRecord cr, int prTot, Date prDate) { 
-		this.changeRecord = cr;
-		this.previousTotal = prTot;
-		this.previousDate = prDate;
-	 }
 }
